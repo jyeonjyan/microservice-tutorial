f6ec38cdb9bb4ffd9ca90cebadee7a539e5ad40c
johnjihwan <s20062@gsm.hs.kr>
2022-02-02 15:26:36 +0900

    Add pattern 을 통한 api-gateway 처리

--- spring-gateway/candy/src/main/java/com/example/candy/CandyController.java
+++ spring-gateway/candy/src/main/java/com/example/candy/CandyController.java
@@ -1,15 +1,18 @@
 package com.example.candy;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
-@RequestMapping("/v1/api")
+@RequestMapping("/candy/v1/api")
 public class CandyController {
 
     @GetMapping("/sayHello")
     public String sayHello(){
+        log.info("======http request======");
         return "hello candy server!";
     }
 
--- spring-gateway/gateway/src/main/java/com/example/gateway/config/GatewayFilterConfig.java
+++ spring-gateway/gateway/src/main/java/com/example/gateway/config/GatewayFilterConfig.java
@@ -12,14 +12,16 @@ public class GatewayFilterConfig {
     public RouteLocator gatewayRoutes(RouteLocatorBuilder builder){
         return builder.routes()
                 .route(r -> r.path("/every-moment/**")
-                            .filters(f -> f.addRequestHeader("every-moment-request", "every-moment-request")
-                                            .addResponseHeader("every-moment-response", "every-moment-response")
-                            ).uri("http://localhost:9200")
+//                            .filters(f -> f.addRequestHeader("every-moment-request", "every-moment-request")
+//                                            .addResponseHeader("every-moment-response", "every-moment-response")
+//                            )
+                        .uri("http://localhost:9200")
                 )
                 .route(r -> r.path("/candy/**")
-                            .filters(f -> f.addRequestHeader("candy-request", "candy-request")
-                                            .addResponseHeader("candy-response", "candy-response")
-                            ).uri("http://localhost:9201")
+//                            .filters(f -> f.addRequestHeader("candy-request", "candy-request")
+//                                            .addResponseHeader("candy-response", "candy-response")
+//                            )
+                        .uri("http://localhost:9201")
                 )
                 .build();
     }
